c9e16308ff85d341a7353cb82f4e6d2369ff4e9c
MissBlumarine <miss_blumarine01@mail.ru>
2022-11-10 16:34:41 +0300

    добавлены классы Cart (вкл методы для него)                  CartAddProductForm

--- boardgameshop/boardgameshop/settings.py
+++ boardgameshop/boardgameshop/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+CART_SESSION_ID = 'cart'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -37,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'myauth.apps.MyauthConfig',
-    'boardgames.apps.BoardgamesConfig'
+    'boardgames.apps.BoardgamesConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
